ca74c07ef25d59a95990b008fd25c6260b98cb3d
Ramon Pocon <pocon.ri@gmail.com>
2023-10-19 09:36:26 -0600

    🚀 update translations

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -1,12 +1,29 @@
-import Data from "@data/sections/services.json";
+import { useEffect } from 'react';
+import { useLanguage } from '../../stores/use-languaje';
+import {getData} from '../../stores/getData';
+import {useAppData} from '../../stores/use-app-data';
 import Link from "next/link";
 
 const ServicesSection = () => {
+    const {isSpanish} = useLanguage();
+  const {sectionData, setSectionData} = useAppData((
+    {sectionData,
+        setSectionData}
+  )=>({sectionData,
+    setSectionData}));
+
+  useEffect(() => {
+      getData('services_section').then((res) => {
+        console.log(res,'res')
+        setSectionData(res)
+      })
+  }, [])
+
   return (
     <section className="gap service-style-one">
         <div className="container">
         <div className="row">
-            {Data.items.map((item, key) => (
+            {(isSpanish ? sectionData?.es : sectionData?.en)?.items.map((item, key) => (
             <div key={`services-item-${key}`} className="col-lg-4 col-md-6 col-sm-12 text-center" >
             <div className="service-data">
                 <div className="svg-icon d-flex-all">
--- src/stores/use-app-data.js
+++ src/stores/use-app-data.js
@@ -3,6 +3,9 @@ import {create} from 'zustand';
 export const useAppData = create((set) => ({
     data: null,
     about: null,
+    sectionData: null,
     setData: (data) => set({data}),
-    setAbout:(about)=>set({about})
+    setAbout:(about)=>set({about}),
+    setSectionData:(sectionData)=>set({sectionData}),
+
 }));
